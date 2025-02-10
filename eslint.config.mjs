b1ded7671244000a57cf01ce2ff5494f8162b818
Augusta Bhardwaj <sars@MacBookAir.lan>
2025-02-11 02:46:12 +0530

    third commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // Allow the use of `any`
+      'react/display-name': 'off', // Disable the missing display name rule
+    },
+  }),
 ];
 
 export default eslintConfig;
